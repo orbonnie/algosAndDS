7b4d1c95ca9ff86c838c866b3d6f7b718eb76108
bonnie <arlolocos@gmail.com>
2022-05-15 14:00:30 -0400

    use frequencyCounter to check for anagrams

---
+++ anagrams.js
@@ -0,0 +1,22 @@
+//given two strings determine if the second string is
+//  is an anagram of the first
+
+const anagrams = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+
+  let charCounter1 = {};
+  let charCounter2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let char1 = str1[i];
+    let char2 = str2[i];
+    charCounter1[char1] = (charCounter1[char1] || 0) + 1;
+    charCounter2[char2] = (charCounter2[char2] || 0) + 1;
+  }
+
+  for (let key in charCounter1) {
+    if (!(charCounter2[key])) return false;
+    if (charCounter2[key] !== charCounter1[k]) return false;
+  }
+  return true;
+}
